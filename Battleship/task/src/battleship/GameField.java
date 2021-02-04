395044ace4e176090cc5af5faea21dd10a88802a
Victoria Grinberg <31312528+ramquist@users.noreply.github.com>
2021-02-04 20:21:09 +0300

    Add Singleton for working with game field

---
+++ Battleship/task/src/battleship/GameField.java
@@ -0,0 +1,47 @@
+package battleship;
+
+/**
+ * Singleton Lazy Initialization
+ */
+public class GameField {
+    private static GameField gameField;
+    private static String[][] field = makeField();
+
+    private GameField() {}
+
+    public static synchronized GameField getGameField() {
+        if (gameField == null) {
+            gameField = new GameField();
+        }
+        return gameField;
+    }
+
+    public static String[][] makeField() {
+        field = new String[11][11];
+        char start = 'A';
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (i == 0 && j == 0) {
+                    field[i][j] = " ";
+                }
+                else if (i == 0) {
+                    field[i][j] = String.valueOf(j);
+                } else if (j == 0){
+                    field[i][j] = String.valueOf(start++);
+                } else {
+                    field[i][j] = "~";
+                }
+            }
+        }
+        return field;
+    }
+
+    public static void printGameField() {
+        for (String[] x : field) {
+            for (String y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -3,6 +3,7 @@ package battleship;
 public class Main {
 
     public static void main(String[] args) {
-        // Write your code here
+        GameField gameField = GameField.getGameField();
+        gameField.printGameField();
     }
 }
